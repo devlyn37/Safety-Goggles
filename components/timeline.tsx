1e8b09b91bd2252c96fa1ff1a23963a6b81dbaf0
devlyn37 <dev37-2009@hotmail.com>
2021-10-18 19:28:50 -0700

    Cleaned up event cards, adjusted styling.

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -182,7 +182,11 @@ const EventGrouping: FC<{ grouping: NFTEvent[] }> = ({ grouping }) => {
 
   return (
     <>
-      <EventCard key={event.key} imgUrl={event.collectionImgUrl}>
+      <EventCard
+        key={event.key}
+        imgUrl={event.collectionImgUrl}
+        boxShadow={true}
+      >
         <div className={styles.titleContainer}>
           <h3 className={styles.title}>
             {event.action} {grouping.length} NFTs{" "}
@@ -238,54 +242,62 @@ const EventGrouping: FC<{ grouping: NFTEvent[] }> = ({ grouping }) => {
 const EventList: FC<{ grouping: NFTEvent[] }> = ({ grouping }) => (
   <>
     {grouping.map((event) => (
-      <EventCard key={event.key} imgUrl={event.assetImgUrl}>
-        <div className={styles.titleContainer}>
-          <h3 className={styles.title}>
-            {event.action}{" "}
-            <a className={styles.link} target="_blank" href={event.assetUrl}>
-              {event.assetName}
-            </a>
-          </h3>
-          <h4 className={styles.subTitle}>
-            <a target="_blank" href={event.collectionUrl}>
-              {event.collectionName}{" "}
-            </a>
-          </h4>
-        </div>
-        <div className={styles.detailContainer} style={{ marginTop: "10px" }}>
-          <div>
-            <div
-              style={{
-                marginBottom: "5px",
-              }}
-              className={styles.detail}
-            >
-              {event.action === "Bought" || event.action === "Sold" ? (
-                weiToEth(event.price) + " ETH"
-              ) : (
-                <a
-                  className={styles.link}
-                  target="_blank"
-                  href={"https://etherscan.io/tx/" + event.transactionHash}
-                >
-                  View on Etherscan
-                </a>
-              )}
-            </div>
-            <div className={styles.subDetail}>{event.date}</div>
-          </div>
-        </div>
-      </EventCard>
+      <Event event={event} />
     ))}
   </>
 );
 
+const Event: FC<{ event: NFTEvent }> = ({ event }) => (
+  <EventCard key={event.key} imgUrl={event.assetImgUrl}>
+    <div className={styles.titleContainer}>
+      <h3 className={styles.title}>
+        {event.action}{" "}
+        <a className={styles.link} target="_blank" href={event.assetUrl}>
+          {event.assetName}
+        </a>
+      </h3>
+      <h4 className={styles.subTitle}>
+        <a target="_blank" href={event.collectionUrl}>
+          {event.collectionName}{" "}
+        </a>
+      </h4>
+    </div>
+    <div className={styles.detailContainer} style={{ marginTop: "10px" }}>
+      <div>
+        <div
+          style={{
+            marginBottom: "5px",
+          }}
+          className={styles.detail}
+        >
+          {event.action === "Bought" || event.action === "Sold" ? (
+            weiToEth(event.price) + " ETH"
+          ) : (
+            <a
+              className={styles.link}
+              target="_blank"
+              href={"https://etherscan.io/tx/" + event.transactionHash}
+            >
+              View on Etherscan
+            </a>
+          )}
+        </div>
+        <div className={styles.subDetail}>{event.date}</div>
+      </div>
+    </div>
+  </EventCard>
+);
+
 const EventCard: FC<{
   imgUrl: string;
-}> = ({ children, imgUrl }) => (
+  boxShadow?: boolean;
+}> = ({ children, imgUrl, boxShadow }) => (
   <div
     className={styles.eventCard}
-    style={{ border: "4px solid black", boxShadow: "0.3rem 0.7rem black" }}
+    style={{
+      border: "4px solid black",
+      boxShadow: boxShadow ? "0.3rem 0.7rem black" : "",
+    }}
   >
     <img className={styles.eventImg} src={imgUrl} loading="lazy"></img>
     <div
